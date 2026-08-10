5f0ecf97739a30aae88afb246487d596d28cef7c
mike485 <michaelgknox@gmail.com>
2026-08-10 15:34:45 -0400

    updated vm with tags and instance type

--- main.tf
+++ main.tf
@@ -9,4 +9,23 @@ terraform {
 
 provider "google" {
   project = "gcp-networking-489914"
+  region = "us-east1"
+  zone = "us-east1-b"
+}
+resource "google_compute_instance" "terraform" {
+  name         = "terraform-vm-1"
+  machine_type = "e2-micro"
+  tags = ["web", "dev"] // Adding network tags to the instance
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  allow_stopping_for_update = true 
+  // Allow to update instance without stopping it
 }
