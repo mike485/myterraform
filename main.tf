10eda010c2d6550a8fbc5ec8c68bb369204a456f
mike485 <michaelgknox@gmail.com>
2026-08-11 15:38:25 -0400

    created vm.tf

--- main.tf
+++ main.tf
@@ -12,20 +12,3 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
-resource "google_compute_instance" "terraform" {
-  name         = var.instance_name
-  machine_type = var.machine_type
-  tags = var.tags // Adding network tags to the instance
-  boot_disk {
-    initialize_params {
-      image = var.image
-    }
-  }
-  network_interface {
-    network = var.network
-    access_config {
-    }
-  }
-  allow_stopping_for_update = true 
-  // Allow to update instance without stopping it
-}
