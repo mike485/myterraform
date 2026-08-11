301b62d81e84d543ccae5ef5e08538ce06f6368a
mike485 <michaelgknox@gmail.com>
2026-08-11 11:06:03 -0400

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,30 @@
-output "network_IP" {
-  value = google_compute_instance.vm_instance.instance_id
-  description = "The internal ip address of the instance"
+output "instance_name" {
+  description = "Name of the compute instance"
+  value       = google_compute_instance.terraform.name
 }
-output "instance_link" {
-  value = google_compute_instance.vm_instance.self_link
-  description = "The URI of the created resource."
+
+output "instance_id" {
+  description = "Provider-assigned ID of the instance"
+  value       = google_compute_instance.terraform.id
 }
+
+output "self_link" {
+  description = "Self-link URL for the instance"
+  value       = google_compute_instance.terraform.self_link
+}
+
+output "external_ip" {
+  description = "External (NAT) IP address assigned to the instance, if any"
+  value       = try(google_compute_instance.terraform.network_interface[0].access_config[0].nat_ip, "")
+}
+
+output "internal_ip" {
+  description = "Internal network IP of the instance"
+  value       = try(google_compute_instance.terraform.network_interface[0].network_ip, "")
+}
+
+output "zone" {
+  description = "Zone where the instance is provisioned"
+  value       = google_compute_instance.terraform.zone
+}
+
